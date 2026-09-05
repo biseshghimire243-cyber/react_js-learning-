2d9092418241edbef20621d14119ca1a336807e5
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-09-05 08:10:07 -0700

    Initialize React learning project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <div>
+            <h1>Hello React!</h1>
+            <p>My React learning journey has started.</p>
+        </div>
+    );
+}
+
+export default App;
