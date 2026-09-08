ecefa3a0c0b2d8f0081e0513190e086e425540bd
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-09-08 05:56:23 -0700

    the new component are done

---
+++ src/03-components/program-01-basic.jsx
@@ -0,0 +1,13 @@
+function Welcome() {
+    return <h1>Welcome to React!</h1>;
+}
+
+function App() {
+    return (
+        <div>
+            <Welcome />
+        </div>
+    );
+}
+
+export default App;
