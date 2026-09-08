62ca2bad20f139e75b6df905c1d1b4161418b164
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-09-08 05:58:12 -0700

    the new are done

---
+++ src/03-components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <header>
+            <h1>React Learning</h1>
+            <p>My React.js Journey</p>
+        </header>
+    );
+}
+
+export default Header;
