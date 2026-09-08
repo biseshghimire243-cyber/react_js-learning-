ec374bab1be921ac5b91e35914530453dfb780e9
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-09-08 05:59:07 -0700

    the new are masde

---
+++ src/03-components/Profile.jsx
@@ -0,0 +1,11 @@
+function Profile() {
+    return (
+        <section>
+            <h2>Bishesh Ghimire</h2>
+            <p>B.Sc. CSIT Student</p>
+            <p>Learning React.js</p>
+        </section>
+    );
+}
+
+export default Profile;
