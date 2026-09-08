3c09180f9f6de4dfe6a53ff151ef363a723a8c84
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-09-08 06:01:21 -0700

    the new practice are mad e'gu

---
+++ src/03-components/program-03-separate.jsx
@@ -0,0 +1,15 @@
+import Header from "./Header.jsx";
+import Profile from "./Profile.jsx";
+import Footer from "./Footer.jsx";
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <Profile />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
