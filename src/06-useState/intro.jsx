f34ce84216e26361a2db3a1c519175c267c7bb95
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-09-15 07:15:39 -0700

    the symbol intro are made

---
+++ src/06-useState/intro.jsx
@@ -0,0 +1,18 @@
+useState
+
+useState is used when we want a React component to remember and update data.
+
+For example:
+
+Counter
+0
+↓
+Click button
+↓
+1
+↓
+Click button
+↓
+2
+
+Without useState, React won't automatically update the screen when the value changes.
