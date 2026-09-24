663a5683150fb83f2ae3a564f334d4c152f33563
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-09-24 05:01:44 -0700

    the footer component are made

---
+++ src/03-components/footer/footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <footer>
+            <p>
+                © 2026 My React Website
+            </p>
+        </footer>
+    );
+}
+
+export default Footer;
