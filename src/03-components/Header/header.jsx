d63980e6c32af3d5fe82b9b16bdcd5972b537d20
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-09-24 05:00:27 -0700

    the hesder are made well

---
+++ src/03-components/Header/header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+    return (
+        <header>
+            <h1>My React Website</h1>
+
+            <nav>
+                <a href="/">Home</a>
+                <a href="/about">About</a>
+                <a href="/contact">Contact</a>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
