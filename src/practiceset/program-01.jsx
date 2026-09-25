caac1b073c509f43f6798fe7a8340acb404d272b
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-09-25 07:31:58 -0700

    the functional, compoment are made

---
+++ src/practiceset/program-01.jsx
@@ -0,0 +1,10 @@
+function Welcome() {
+    return (
+        <div>
+            <h1>Welcome to React</h1>
+            <p>I am practicing React components.</p>
+        </div>
+    );
+}
+
+export default Welcome;
