8ab508614c321e08e77e68a8faa6de8ffa36ce08
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-09-25 07:34:42 -0700

    the secong fprogram ar amdded

---
+++ src/practiceset/program-02.jsx
@@ -0,0 +1,14 @@
+// program-02.jsx
+
+function StudentInfo() {
+    return (
+        <div>
+            <h1>Student Information</h1>
+            <p>Name: Bishesh Ghimire</p>
+            <p>Course: B.Sc. CSIT</p>
+            <p>Semester: 8th Semester</p>
+        </div>
+    );
+}
+
+export default StudentInfo;
