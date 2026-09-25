39ddddd03e280e26253063475a6cc0d293c18e8f
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-09-25 07:35:47 -0700

    the third proram are amdeed well

---
+++ src/practiceset/program-03.jsx
@@ -0,0 +1,14 @@
+// program-03.jsx
+
+function ProductInfo() {
+    return (
+        <div>
+            <h1>Product Information</h1>
+            <p>Product: Laptop</p>
+            <p>Price: Rs. 85000</p>
+            <p>Category: Electronics</p>
+        </div>
+    );
+}
+
+export default ProductInfo;
