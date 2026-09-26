db9e7c265875aedcb91806ec2d384e68b36b89d6
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-09-26 03:41:25 -0700

    the initial commit

---
+++ src/practiceset/program-05.jsx
@@ -0,0 +1,21 @@
+// program-05.jsx
+
+function ProfileInfo() {
+    return (
+        <div>
+            <h2>Bishesh Ghimire</h2>
+            <p>B.Sc. CSIT Student</p>
+        </div>
+    );
+}
+
+function Profile() {
+    return (
+        <div>
+            <h1>My Profile</h1>
+            <ProfileInfo />
+        </div>
+    );
+}
+
+export default Profile;
